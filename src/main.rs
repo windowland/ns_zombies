86d1ef532b0da07f986f77771a868dfef81218de
windowland <windowlandns@gmail.com>
2022-11-02 12:07:40 -0500

    removed krasnoyarsk

--- src/main.rs
+++ src/main.rs
@@ -35,8 +35,6 @@ fn main() -> Result<(), Box<dyn Error>> {
     let mut map = graph.get_stats();
     let forest = map.values().cloned().sum();
     map.insert("forest", forest);
-    let krasnoyarsk = graph.get_stats_regex(&Regex::new(r"krasnoyarsk\-[0-9]+")?);
-    map.insert("krasnoyarsk-*", krasnoyarsk);
     let can = graph.get_stats_regex(&Regex::new(r"can\-([0-9]+)|(founder)")?);
     map.insert("can-*", can);
     map.into_iter()
