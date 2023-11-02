e940cf91c0a95788b6b9a243089c1c85dc988263
windowland <windowlandns@gmail.com>
2023-11-01 19:04:38 -0500

    cargo update

--- src/happenings.rs
+++ src/happenings.rs
@@ -2,7 +2,7 @@ use serde::{Deserialize, Serialize};
 #[derive(Serialize, Deserialize, Debug, Clone, Eq, PartialEq)]
 #[serde(rename_all = "UPPERCASE")]
 pub struct Event {
-    #[serde(rename = "id")]
+    #[serde(rename = "@id")]
     pub id: u64,
     pub timestamp: u64,
     pub text: String,
